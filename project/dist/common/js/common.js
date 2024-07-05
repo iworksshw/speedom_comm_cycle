13d4beb998b6f354ae5eb1fab44fcadc83cd9715
jonatkim <53809773+jonatkim@users.noreply.github.com>
2024-07-05 15:54:29 +0900

    20240705 update1

--- project/dist/common/js/common.js
+++ project/dist/common/js/common.js
@@ -28,9 +28,137 @@ document.addEventListener("DOMContentLoaded", function () {
             fileBox.classList.remove("on");
         });
     });
+
+    accordionInit(); //아코디언 함수
+    tabMenuInit();   //탭메뉴 함수
 });
 
+// ------------------------------- 아코디언 함수 ------------------------------- //
+function accordionInit(){
+    const modAccos = document.querySelectorAll(".modAccordion");
+    modAccos.forEach(function(modAcco,modIdx){
+        const accoItems = modAcco.querySelectorAll(".accrBox");
+        accoItems.forEach(function(accoItem,itemIdx,elements){
+            const accoBtn = accoItem.querySelector(".accrTitle");
+            const accoCont = accoItem.querySelector(".accrBody");
+            accoBtn.addEventListener("click",function(){
+                if(this.classList.contains("on")){
+                    this.classList.remove("on");
+                    slideUp(accoCont,300);
+                    return;
+                }
+                this.classList.add("on");
+                slideDown(accoCont,300);
+            });
+        });
+    });
+}
+
+// ------------------------------- 탭메뉴 함수 ------------------------------- //
+//모드 탭 함수
+function tabMenuInit(){
+    //모드탭의 수
+    const modTabs = document.querySelectorAll(".modTab");
+    modTabs.forEach(function(modTab,tabIdx,elements){
+        const tabmenus = modTab.querySelectorAll(".tabName");
+        const tabConts = modTab.querySelectorAll(".tabCont");
+
+        //모드탭 내의 메뉴 수
+        tabmenus.forEach(function(tabmenu,menuIdx,inElements){
+            tabmenu.addEventListener("click", function(event){
+                inElements.forEach(function(inElement){
+                    inElement.classList.remove("on");
+                    inElement.setAttribute("title", "탭메뉴");
+                });
+                this.classList.add("on");
+                this.setAttribute("title", "선택 된 탭메뉴");
+                tabConts.forEach(function(tabCont,contIdx){
+                    tabCont.classList.remove("on");
+                    if(menuIdx == contIdx){
+                        tabCont.classList.add("on");
+                    }
+                })
+            });
+        });
+    });
+}
+
+// ------------------------------- 모션 함수 ------------------------------- //
+
+//fade in
+function fadeIn(element, duration){
+    let opacity = 0;
+    element.style.display = "block";
+    element.style.opacity = opacity;
+    let action = setInterval(function(){
+        opacity += 10/duration;
+        element.style.opacity = opacity;
+        if(opacity >= 1){
+            clearInterval(action);
+        }
+    }, 10);
+}
+
+//fade out
+function fadeOut(element, duration){
+    let opacity = 1;
+    element.style.opacity = opacity;
+    let action = setInterval(function(){
+        opacity -= 10/duration;
+        element.style.opacity = opacity;
+        if(opacity <= 0){
+            clearInterval(action);
+            element.style.display = "none";
+        }
+    }, 10);
+}
+
+//fade toggle
+function fadeToggle(element, duration){
+    element.style.display == "block" ? fadeOut(element, duration) : fadeIn(element, duration);
+}
+
+//slide function (slideUp)
+function slideUp(element, duration) {
+    let height = element.scrollHeight;
+    let interval = 10; // millidurationonds per frame
+    let steps = Math.ceil(duration / interval);
+    let stepHeight = height / steps;
+    let currentStep = 0;
+
+    let slideUpInterval = setInterval(function() {
+        currentStep++;
+        element.style.height = (height - stepHeight * currentStep) + "px";
+        if (currentStep >= steps) {
+            clearInterval(slideUpInterval);
+            element.style.display = "none";
+            element.style.height = ""; // 높이를 초기화하여 다시 제대로 작동하도록 합니다.
+        }
+    }, interval);
+}
+
+//slide function (slideDown)
+function slideDown(element, duration) {
+    element.style.display = "block";
+    element.style.height = "0px"; // 이 부분을 수정하여 초기 높이를 0으로 설정합니다.
+    let height = element.scrollHeight;
+    let interval = 10; // millidurationonds per frame
+    let steps = Math.ceil(duration / interval);
+    let stepHeight = height / steps;
+    let currentStep = 0;
+
+    let slideDownInterval = setInterval(function() {
+        currentStep++;
+        element.style.height = (stepHeight * currentStep) + "px";
+        if (currentStep >= steps) {
+            clearInterval(slideDownInterval);
+            element.style.height = ""; // 높이를 초기화하여 다시 제대로 작동하도록 합니다.
+        }
+    }, interval);
+}
+
 // ------------------------------- 기타 함수 ------------------------------- //
+
 //parents 엘리먼트 찾기
 function parentsElementFind(my, findElement){
     let parentElement = my.parentNode;
@@ -42,3 +170,79 @@ function parentsElementFind(my, findElement){
         }
     }
 }
+
+// ------------------------------- datePicker ------------------------------- //
+//날짜
+function datePicker(startIpt, startCont){
+    let datepicker = new tui.DatePicker('#'+startCont, {
+        date: new Date(),
+        input: {
+            element: '#'+ startIpt,
+            format: 'yyyy-MM-dd'
+        }
+    });
+}
+//날짜 + 시간
+function timePicker(startIpt, startCont){
+    let datepicker = new tui.DatePicker('#'+startCont, {
+        date: new Date(),
+        input: {
+            element: '#'+ startIpt,
+            format: 'yyyy-MM-dd HH:mm A'
+        },
+        timePicker: true
+    });
+}
+//datePicker("datepicker-input","wrapper");
+
+//날짜 기간~기간
+function datePickerTo(startIpt, startCont, endIpt, endCont){
+    let today = new Date();
+    let picker = tui.DatePicker.createRangePicker({
+        startpicker: {
+            date: today,
+            input: '#'+ startIpt,
+            container: '#'+ startCont
+        },
+        endpicker: {
+            date: today,
+            input: '#'+ endIpt,
+            container: '#'+ endCont
+        },
+        selectableRanges: [
+            [today, new Date(today.getFullYear() + 1, today.getMonth(), today.getDate())]
+        ],
+        format: 'YYYY-MM-dd'
+    });
+
+    picker.on('change:end', () => {
+        console.log(123);
+    })
+}
+//날짜 기간+시간 ~ 기간+시간
+function timePickerTo(startIpt, startCont, endIpt, endCont){
+    let today = new Date();
+    let picker = tui.DatePicker.createRangePicker({
+        startpicker: {
+            date: today,
+            input: '#'+ startIpt,
+            container: '#'+ startCont
+        },
+        endpicker: {
+            date: today,
+            input: '#'+ endIpt,
+            container: '#'+ endCont
+        },
+        selectableRanges: [
+            [today, new Date(today.getFullYear() + 1, today.getMonth(), today.getDate())]
+        ],
+        format: 'YYYY-MM-dd HH:mm',
+        timePicker: true
+    });
+
+    picker.on('change:end', () => {
+        console.log(123);
+    })
+}
+
+//datePickerTo("startpicker-input", "startpicker-container", "endpicker-input", "endpicker-container");
